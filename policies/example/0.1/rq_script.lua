81194e2c3c159f055389952466808bbdb5d398cd
Tin Hanchanachai <tinhanc@gmail.com>
2019-06-10 15:08:13 +0700

    Update rq_script.lua

--- policies/example/0.1/rq_script.lua
+++ policies/example/0.1/rq_script.lua
@@ -4,6 +4,10 @@ local _M = require('apicast.policy').new('Gen UUID', '0.1')
 local mt = { __index = _M }
 
 function _M.new()
+  return setmetatable({}, mt)
+end
+
+function _M:rewrite()
   local config = configuration or {}
   local set_header = config.set_header or {}
   local random = math.random
@@ -12,11 +16,9 @@ function _M.new()
         local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
         return string.format('%x', v) end)
   
-  ngx.log(0, 'setting header: x-request-id : ', rq_uuid)
   ngx.req.set_header('x-request-id', rq_uuid)
---  ngx.log(0, ngx.req.raw_header())
   ngx.log(0, 'x-request-id : ', rq_uuid, ', req_body : ', ngx.var.request_body)
-  return setmetatable({}, mt)
+  
 end
 
 function _M:body_filter()
@@ -27,7 +29,6 @@ function _M:body_filter()
       resp = ngx.ctx.buffered
     end
 
---    ngx.log(0, ngx.req.raw_header())
     ngx.log(0, 'x-request-id : ', rq_uid, ', resp_body : ', resp)
   
 end
