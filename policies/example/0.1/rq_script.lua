324452cefc8a3c99cfeb46685e2b447bd556e2c5
Tin Hanchanachai <tinhanc@gmail.com>
2019-06-10 14:21:02 +0700

    Update rq_script.lua

--- policies/example/0.1/rq_script.lua
+++ policies/example/0.1/rq_script.lua
@@ -6,15 +6,15 @@ local mt = { __index = _M }
 function _M.new()
   local config = configuration or {}
   local set_header = config.set_header or {}
-  
+  ngx.log(ngx.NOTICE, 'Gen UUID');
   local random = math.random
   local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
   local rq_uuid = string.gsub(template, '[xy]', function (c)
         local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
         return string.format('%x', v) end)
   
-  ngx.log(ngx.NOTICE, 'setting header: ', 'x-request-id', ' to: ', rq_uuid)
-  ngx.log(ngx.NOTICE, 'req_body: ', ngx.var.request_body, ' rq_uuid : ', rq_uuid)
+  ngx.log(ngx.NOTICE, 'setting header: x-request-id : ', rq_uuid)
+  --ngx.log(ngx.NOTICE, 'req_body: ', ngx.var.request_body, ' rq_uuid : ', rq_uuid)
   ngx.req.set_header('x-request-id', rq_uuid)
   return setmetatable({}, mt)
 end
