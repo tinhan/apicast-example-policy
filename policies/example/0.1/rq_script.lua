aaef524a31ab6f5733239bde49876e4af4d0641c
Tin Hanchanachai <tinhanc@gmail.com>
2019-06-10 14:36:45 +0700

    Update rq_script.lua

--- policies/example/0.1/rq_script.lua
+++ policies/example/0.1/rq_script.lua
@@ -6,23 +6,23 @@ local mt = { __index = _M }
 function _M.new()
   local config = configuration or {}
   local set_header = config.set_header or {}
-  ngx.log(ngx.NOTICE, 'Gen UUID');
   local random = math.random
   local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
   local rq_uuid = string.gsub(template, '[xy]', function (c)
         local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
         return string.format('%x', v) end)
+  
   ngx.req.set_header('x-request-uid', rq_uuid)
   ngx.log(0, 'setting header: x-request-id : ', rq_uuid)
-  ngx.log(0, ngx.req.raw_header())
-  ngx.log(0, 'req_body: ', ngx.var.request_body, ' rq_uuid : ', rq_uuid)
+  ngx.log(0, ngx.req.get_headers(), 'req_body: ', ngx.var.request_body)
+  --ngx.log(0, 'req_body: ', ngx.var.request_body, ' rq_uuid : ', rq_uuid)
   return setmetatable({}, mt)
 end
 
 function _M:post_action()
   -- do something after the response was sent to the client
-  ngx.log(0, ngx.resp.get_headers())
-  ngx.log(0, 'req_body: ', ngx.var.response_body)
+  ngx.log(0, ngx.resp.get_headers(), 'res_body: ', ngx.var.response_body)
+  -- ngx.log(0, 'req_body: ', ngx.var.response_body)
 end
 
 return _M
